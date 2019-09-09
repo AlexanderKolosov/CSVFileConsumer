af8834aeae79b5bbc162a33cf66e378a9b274401
Alexander <alexkp@inbox.ru>
2019-09-09 13:48:51 +0300

    CSVFileParser parse first line SQLiteDBFactory creates table

---
+++ src/main/java/net/optimalsolutionshub/csvfileconsumer/model/CSVFileParser.java
@@ -0,0 +1,28 @@
+package net.optimalsolutionshub.csvfileconsumer.model;
+
+import au.com.bytecode.opencsv.CSVReader;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.SQLException;
+
+public class CSVFileParser {
+    private static String CSVfilePath = "D:/Java/Test/Interview-task-data-osh.csv";
+
+    public static void parseCSVFile() throws IOException, SQLException {
+        CSVReader reader = new CSVReader(new FileReader(CSVfilePath));
+
+        String[] nextLine = reader.readNext();
+        SQLiteDBFactory.createCustomerXTable(nextLine);
+    }
+
+    public static void main(String[] args) {
+        try {
+            CSVFileParser.parseCSVFile();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
